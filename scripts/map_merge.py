e0a302b33efda4333559812cd74348eabed7bfa4
Häntze <15168710+hhaentze@users.noreply.github.com>
2024-01-05 18:01:27 +0100

    Refactor map_merge.py. Add function to merge segmentations with charite annotations

--- scripts/map_merge.py
+++ scripts/map_merge.py
@@ -3,64 +3,49 @@ from os.path import join
 import config
 import labelmappings as lm
 import pandas as pd
-from monai.transforms import KeepLargestConnectedComponent, LoadImage, MapLabelValue, SaveImage
+from monai.transforms import LoadImage, MapLabelValue, SaveImage
 from tqdm import tqdm
 
-# Merge first generation of predicitons
-deleteBadTotalClassesV1 = MapLabelValue(
-    orig_labels=[4, 31, 32, 33, 34],
-    target_labels=[0] * 5,
-    dtype=int,
-)
-deleteBadMRClassesV1 = MapLabelValue(
-    orig_labels=list(range(4)) + list(range(5, 31)) + list(range(35, 41)),
-    target_labels=[0] * 36,
-    dtype=int,
-)
-keepLargestMusclesGall = KeepLargestConnectedComponent(applied_labels=[4, 31, 32, 33, 34])
+# Specify channel mappings
+channels_all = set(range(1, 41))
+channels_charite = {1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 14, 15, 20, 21, 22, 23, 31, 32, 33, 34}
+channels_MRSeg_V1 = {4, 31, 32, 33, 34}
+channels_MRSeg_V2 = {4, 10, 11, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40}
 
-# Merge second generation of predicitons
-deleteBadTotalClassesV2 = MapLabelValue(
-    orig_labels=[4, 10, 11, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
-    target_labels=[0] * 19,
-    dtype=int,
-)
-deleteBadMRClassesV2 = MapLabelValue(
-    orig_labels=list(range(1, 4)) + list(range(5, 10)) + list(range(12, 25)),
-    target_labels=[0] * 21,
+# Clean segmentations
+Clean = lambda channels: MapLabelValue(
+    orig_labels=channels,
+    target_labels=[0] * len(channels),
     dtype=int,
 )
 
-
-save = SaveImage(
-    output_dir=join(config.ukbb, "preds_combined2"),
+# Save result
+Save = lambda path: SaveImage(
+    output_dir=join(config.ukbb, path),
     separate_folder=False,
     output_postfix="new",
     print_log=False,
 )
 
 
-def merge_label(totalseg_label, mrseg_label, segmentation_generation):
+def merge_label(bottom_label, top_label, top_label_channels):
+    """Merge two labels. Channels spefied in ´top_label_channels´ will be deleted from the bottom label and vice versa.
+    If channels from both labels overlap, the channel of the top label will be kept.
+    Informal: This function puts the top label "on top" of the bottom label"""
+
     # clean labels
-    if segmentation_generation == 0:
-        totalseg_label = deleteBadTotalClassesV1(totalseg_label)
-        mrseg_label = deleteBadMRClassesV1(mrseg_label)
-    elif segmentation_generation == 1:
-        totalseg_label = deleteBadTotalClassesV2(totalseg_label)
-        mrseg_label = deleteBadMRClassesV2(mrseg_label)
-    else:
-        raise Exception("Unknow segmentation generation")
+    bottom_label = Clean(top_label_channels)(bottom_label)
+    top_label = Clean(channels_all - top_label_channels)(top_label)
 
     # merge labels
-    new_label = mrseg_label.detach().clone()
-    new_label[totalseg_label != 0] = totalseg_label[totalseg_label != 0]
+    new_label = bottom_label.detach().clone()
+    new_label[top_label != 0] = top_label[top_label != 0]
 
-    # remove artifacts that were created during merging
-    new_label = keepLargestMusclesGall(new_label)
     return new_label
 
 
-def main(segmentation_generation):
+def merge_sections_123(segmentation_generation):
+    """Merge predictions of MRSegmentator and TotalSegmentator of sections one, two and three."""
     data = pd.read_csv(join(config.ukbb, "manifest.csv"))
 
     # remove patients with more than 6 scans? Might be erroneous?
@@ -78,22 +63,24 @@ def main(segmentation_generation):
     for _, row in tqdm(data.iterrows(), total=len(data)):
         # Load labels and map to new metadata specification
         file = row["image"].replace("/", "_")
-        if segmentation_generation == 0:
-            label_mr = lm.transform_mrs(join(config.ukbb, "preds_mr", file))
-        elif segmentation_generation == 1:
-            label_mr = LoadImage()(join(config.ukbb, "preds_mr2", file))
-        else:
-            raise Exception("Unknow segmentation generation")
         label_total = lm.transform_totals(join(config.ukbb, "preds_total", file))
 
-        # merge labels
-        new_label = merge_label(label_total, label_mr, segmentation_generation)
+        # Merge labels
+        if segmentation_generation == 0:
+            label_mr = lm.transform_mrs(join(config.ukbb, "preds_mr", file))
+            new_label = merge_label(label_total, label_mr, channels_MRSeg_V1)
+        elif segmentation_generation == 1:
+            label_mr = LoadImage()(join(config.ukbb, "preds_mr2", file))
+            new_label = merge_label(label_total, label_mr, channels_MRSeg_V2)
+        else:
+            raise Exception("Unknow segmentation generation")
 
-        # save label
-        save(new_label)
+        # Save label
+        Save("preds_combined2")(new_label)
 
 
-def map_s0_s4():
+def Clean_sections_04():
+    """Clean predictions of TotalSegmentator of sections zero and four."""
     data = pd.read_csv(join(config.ukbb, "manifest.csv"))
 
     # remove patients with more than 6 scans? Might be erroneous?
@@ -119,10 +106,26 @@ def map_s0_s4():
                 label_total = lm.clean_section4(label_total)
 
             # save label
-            save(label_total)
+            Save("section04")(label_total)
         except FileNotFoundError:
             print("File not found: ", file)
 
 
+def map_merge_charite():
+    """Merge annotations of charite dataset with predictions of remaining channels by MRSegmentator"""
+    data = pd.read_csv(config.ukbb + "csv/charite_annotations.csv")
+
+    for _, row in tqdm(data.iterrows(), total=len(data)):
+        # Load labels
+        annotation = lm.transform_mrs(join(config.mr_label_path, row["label"]))
+        prediction = LoadImage()(join(config.ukbb, "preds_charite", row["pred"]))
+
+        # Merge labels
+        new_label = merge_label(prediction, annotation, channels_charite)
+
+        # save label
+        Save("preds_charite_combined")(new_label)
+
+
 if __name__ == "__main__":
-    main(segmentation_generation=1)
+    map_merge_charite()
